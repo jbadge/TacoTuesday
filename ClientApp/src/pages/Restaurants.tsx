7e797fa1e44113e88bd2461e8390b1069a1856b8
Jeremy Badger <jeremebadger@gmail.com>
2024-07-19 16:26:55 -0700

    Loads a list of restaurants from the api and shows them on the restaurants page

---
+++ ClientApp/src/pages/Restaurants.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { useQuery } from 'react-query'
+
+import map from '../images/map.png'
+import tacoTuesday from '../images/taco-tuesday.svg'
+import { CSSStarsProperties, RestaurantType } from '../types/types'
+
+export function Restaurants() {
+  const { data: restaurants = [] } = useQuery<RestaurantType[]>(
+    'restaurants',
+    async function () {
+      const response = await fetch('/api/restaurants')
+      return response.json()
+    }
+  )
+
+  return (
+    <main className="home">
+      <h1>
+        <img src={tacoTuesday} alt="Taco Tuesday" />
+      </h1>
+      <form className="search">
+        <input type="text" placeholder="Search..." />
+      </form>
+
+      <section className="map">
+        <img alt="Example Map" src={map} />
+      </section>
+
+      <ul className="results">
+        {restaurants.map((restaurant) => (
+          <li key={restaurant.id}>
+            <h2>{restaurant.name}</h2>
+            <p>
+              <span
+                className="stars"
+                style={{ '--rating': 4.7 } as CSSStarsProperties}
+                aria-label="Star rating of this location is 4.7 out of 5."
+              ></span>
+              (2,188)
+            </p>
+            <address>{restaurant.address}</address>
+          </li>
+        ))}
+      </ul>
+    </main>
+  )
+}
---
+++ ClientApp/src/types/types.ts
@@ -0,0 +1,13 @@
+import { CSSProperties } from 'react'
+
+export interface CSSStarsProperties extends CSSProperties {
+  '--rating': number
+}
+
+export type RestaurantType = {
+  id?: number
+  name: string
+  description: string
+  address: string
+  telephone: string
+}
